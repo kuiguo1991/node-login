013c5ac01094d5878ab24b802ee417ff83ec2786
kuiguo1991 <kuiguo1991@gmail.com>
2019-03-12 10:53:56 +0800

    提交2019/03/12#01

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "nodejs-login",
+  "version": "0.1.0",
+  "description": "nodejs 调用api接口",
+  "title": "api接口",
+  "url": "https://192.168.0.116:5551",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-03-12T02:52:44.654Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
--- routers/index.js
+++ routers/index.js
@@ -3,8 +3,28 @@ const router = express.Router();
 
 const loginUsers = require("../controllers/login")
 
+
+/**
+* @api {get} /api/loginUsers/user 用户数据
+* @apiDescription 获取用户数据
+* @apiName user
+* @apiGroup loginUsers
+* @apiVersion 1.0.0
+*/
+
+
 router.get("/loginUsers/user",loginUsers.user)
 
+
+/**
+* @api {post} /api/loginUsers/login 用户登录
+* @apiDescription 用户登录
+* @apiName login
+* @apiGroup loginUsers
+* @apiParam {string} USERNAME 用户名
+* @apiParam {string} PASSWORD 密码
+* @apiVersion 1.0.0
+*/
 router.post("/loginUsers/login",loginUsers.login)
 
 module.exports = router;
