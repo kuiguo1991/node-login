4c6fa9563726e1bd85943cd3977e51b3d113f0f6
kuiguo <kuiguo1991@gmail.com>
2021-01-29 09:56:23 +0800

    2020/01/29

--- jwt.js
+++ jwt.js
@@ -1,98 +1,33 @@
 /*
  * @Author: your name
  * @Date: 2020-09-14 09:30:23
- * @LastEditTime: 2020-09-17 15:37:14
+ * @LastEditTime: 2021-01-29 09:54:02
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \github\node-login\jwt.js
  */
 // 引入模块依赖
-const path = require("path");
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
 
-const NodeRSA = require("node-rsa");
-const crypto = require("crypto");
-const key = new NodeRSA({ b: 2048 }); //生成2048位的密钥
-const publicDer = key.exportKey("pkcs1-public-pem"); //公钥
-const privateDer = key.exportKey("pkcs1-private-pem"); //私钥
-// // 创建 token 类
-// class Jwt {
-//   constructor(data) {
-//     this.data = data;
-//   }
+// 创建 token 类
+// Token 数据
+const payload = {
+  name: "YuGuo",
+  admin: true,
+};
+const secret = "STUDYJWT";
 
-//   //生成token
-//   generateToken() {
-//     let data = this.data;
-//     let created = Math.floor(Date.now() / 1000);
-//     let cert = fs.readFileSync(path.join(__dirname, "./pem/private_key.pem")); //私钥 可以自己生成
-//     let token = jwt.sign(
-//       {
-//         data,
-//         exp: created + 60 * 30,
-//       },
-//       cert,
-//       { algorithm: "RS256" }
-//     );
-//     return token;
-//   }
+// 签发 Token
+const token = jwt.sign(payload, secret, { expiresIn: 60 * 60 * 24 }); //expiresIn设置为24小时过期
 
-//   // 校验token
-//   verifyToken() {
-//     let token = this.data;
-//     let cert = fs.readFileSync(path.join(__dirname, "./pem/public_key.pem"));
-//     let res;
-//     try {
-//       let result = jwt.verify(token, cert, { algorithms: ["RS256"] }) || {};
-//       let { exp = 0 } = result,
-//         current = Math.floor(Date.now() / 1000);
-//       if (current <= exp) {
-//         res = result.data || {};
-//       }
-//     } catch (e) {
-//       res = "err";
-//     }
-//     return res;
-//   }
-// };
+// 输出签发的 Token
+console.log(token);
 
-function generateToken() {
-  let data = {
-    name: "yuguo",
-  };
-  let created = Math.floor(Date.now() / 1000);
-  let token = jwt.sign(
-    {
-      data,
-      exp: created + 60 * 30,
-    },
-    privateDer,
-    { algorithm: "RS256" }
-  );
-  return token;
-}
-
-function verifyToken() {
-  let data = {
-    name: "yuguo",
-  };
-  let res;
-  try {
-    let result = jwt.verify(data, publicDer, { algorithms: ["RS256"] }) || {};
-    console.log(result);
-    let { exp = 0 } = result,
-      current = Math.floor(Date.now() / 1000);
-    if (current <= exp) {
-      res = result.data || {};
-    }
-  } catch (e) {
-    res = "err";
+jwt.verify(token,"STUDYJWT", (error, decoded) => {
+  if (error) {
+    console.log(error.message);
+    return;
   }
-  return res;
-}
-
-// module.exports = Jwt;
-
-// console.log(generateToken());
-console.log(verifyToken());
+  console.log(decoded);
+});
