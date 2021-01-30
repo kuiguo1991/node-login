7b95cd91ee83570e3bc644059b63cc5261d8bb05
kuiguo <kuiguo1991@gmail.com>
2021-01-30 15:34:48 +0800

    增加数据

--- data/login_user.sql
+++ data/login_user.sql
@@ -1,2 +1,3 @@
-insert into login.user (id, name, url, age, area, country) values (1, 112, 'wqeqwe', 0, 'qweqwe', 'eqwe');
-insert into login.user (id, name, url, age, area, country) values (2, 1233, 213213, 0, 3123, 23123);
+insert into login.user (id, name, url, age, area, country, cipher) values (1, 'yuguo', 'wqeqwe', 0, 'qweqwe', 'eqwe', 123456);
+insert into login.user (id, name, url, age, area, country, cipher) values (2, 1233, 213213, 0, 3123, 23123, 12345555);
+insert into login.user (id, name, url, age, area, country, cipher) values (3, 12, 3333, 0, 33, 333, 123333);
--- db/ygSQL.js
+++ db/ygSQL.js
@@ -1,14 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2020-09-14 09:30:23
- * @LastEditTime: 2021-01-30 11:20:35
+ * @LastEditTime: 2021-01-30 15:31:04
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \github\node-login\db\ygSQL.js
  */
 module.exports = {
   queryAllSQL: "SELECT * FROM user",
-  LoginSQL: "SELECT * FROM user WHERE USERNAME = ? AND PASSWORD = ?",
+  LoginSQL: "SELECT * FROM user WHERE name = ? AND cipher = ?",
   addSQL:
     "INSERT INTO websites(id, name, url, area, country) VALUES(0,?,?,?,?)",
   modSQL:
